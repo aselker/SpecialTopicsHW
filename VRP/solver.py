4732df2b86acb056bf8ac9e5b9d3d42f252c7cac
Adam Selker <adamselker@gmail.com>
2020-04-16 17:03:46 -0700

    Why do I always forget to do a separate commit for formatting and crunchbang

--- VRP/solver.py
+++ VRP/solver.py
@@ -1,15 +1,15 @@
-'''Code file for vehicle routing problem created for Advanced Algorithms
+"""Code file for vehicle routing problem created for Advanced Algorithms
 Spring 2020 at Olin College. These functions solve the vehicle routing problem
 using an integer programming and then a local search approach. This code has
-been adapted from functions written by Alice Paul.'''
+been adapted from functions written by Alice Paul."""
 
 import picos as pic
 import numpy as np
 from read_files import read_file_type_A, read_file_type_C
 
 # Integer programming approach
-def cvrp_ip(C,q,K,Q,obj=True):
-    '''
+def cvrp_ip(C, q, K, Q, obj=True):
+    """
     Solves the capacitated vehicle routing problem using an integer programming
     approach.
 
@@ -21,8 +21,10 @@ def cvrp_ip(C,q,K,Q,obj=True):
     returns:
         objective_value: value of the minimum travel cost
         x: matrix representing number of routes that use each arc
-    '''
+    """
     # TODO: add in destination node (same distances as source & demand = 0)
+    C = deepcopy(C)  # Just in case
+    print(C)
 
     # set up the picos problem
     prob = pic.Problem()
@@ -34,9 +36,10 @@ def cvrp_ip(C,q,K,Q,obj=True):
 
     return objective_value, x
 
+
 # Local search approach (OPTIONAL)
-def local_search(C,q,K,Q):
-    '''
+def local_search(C, q, K, Q):
+    """
     Solves the capacitated vehicle routing problem using a local search
     approach.
 
@@ -47,7 +50,7 @@ def local_search(C,q,K,Q):
     returns:
         bestval: value of the minimum travel cost
         bestx: matrix representing number of routes that use each arc
-    '''
+    """
     bestx = []
     bestval = 0
 
@@ -59,6 +62,6 @@ def local_search(C,q,K,Q):
 if __name__ == "__main__":
 
     # an example call to test your integer programming implementation
-    C,q,K,Q = read_file_type_A('data/A-n05-k04.xml')
-    travel_cost, x = cvrp_ip(C,q,K,Q)
+    C, q, K, Q = read_file_type_A("data/A-n05-k04.xml")
+    travel_cost, x = cvrp_ip(C, q, K, Q)
     print("Travel cost: " + str(travel_cost))
